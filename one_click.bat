304b16196019131e84b712c507af637b5d94b2e7
Li_Peter <505175463@qq.com>
2023-07-21 01:51:47 +0800

    Clear Git cache

--- one_click.bat
+++ one_click.bat
@@ -3,7 +3,7 @@ cd C:\Users\50517\Desktop\xiaogpt
 set MI_USER=18024490465
 set MI_PASS=peter!971213
 set MI_DID=568651665
-set OPENAI_API_KEY=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
+set OPENAI_API_KEY=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
 
 start /B /HIGH python xiaogpt.py --hardware LX04 --use_chatgpt_api --use_command --mute_xiaoai
 
