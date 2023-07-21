b0223b6991e5f314abce1311aaaba68874117d8d
Li_Peter <505175463@qq.com>
2023-07-21 12:07:37 +0800

    pythonw -u

--- xiaogpt.py
+++ xiaogpt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from xiaogpt.cli import main
+import time
 
 if __name__ == "__main__":
     while True:
@@ -7,4 +8,6 @@ if __name__ == "__main__":
             main()
         except Exception as e:
             print("main方法捕捉到错误:", e)
-            main()
+            print("等待 5 秒后重新运行主程序...")
+            time.sleep(5)
+            continue
